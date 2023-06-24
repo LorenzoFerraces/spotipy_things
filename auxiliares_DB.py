16868ab9d4b711b99129d573271cbbb3e9b22e91
LorenzoFerraces <lorenzoferraces@proton.me>
2023-06-24 20:07:43 -0300

    cambios de comodidad, archivos unificados

--- auxiliares_DB.py
+++ auxiliares_DB.py
@@ -1,33 +1,85 @@
 from configparser import ConfigParser
+import psycopg2  
 
-def config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
+class db_user():
 
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    def __init__(self, filename, section):
+        params = self.__config(filename, section)
+        self.__connection = psycopg2.connect(**params)
+        self.__cur = self.__connection.cursor()
 
-    return db
+    def __config(self, filename, section):
+        # create a parser
+        parser = ConfigParser()
+        # read config file
+        parser.read(filename)
+
+        # get section, default to postgresql
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        return db
+
+    def clear_single_quote(self, string):
+        list = string.split('\'')
+        result = ''.join(list)
+        return result
+
+    def insert_release(self, name_Param, artist_Param, release_date_Param):
+        query =   """INSERT INTO release (name, artist, release_date) VALUES ({name}, {artist}, {release_date}) RETURNING name, artist;""".format(
+            name = name_Param, artist = artist_Param, release_date = release_date_Param)
+        return query
+
+    def insert_genres(self, name_Param, artist_Param, genre_Param):
+        query =   """INSERT INTO genres (name, artist, genre) VALUES ({name}, {artist}, {genre}) RETURNING name, genre;""".format(
+            name = name_Param, artist = artist_Param, genre = genre_Param)
+        return query
+
+    def export_into_release(self, dates):
+        for line in dates:
+            try:
+                raw_Name = line.split(';')[0].strip()
+                name = "\'" + self.clear_single_quote(raw_Name) + "\'"
+                raw_Artist = line.split(';')[1].strip()
+                artist = "\'" + self.clear_single_quote(raw_Artist) + "\'"
+                raw_Date = line.split(';')[2].strip()
+                yyyy_mm_dd = raw_Date.split('-') 
+                if(len(yyyy_mm_dd) < 3):
+                    continue
+                date = "\'" + self.clear_single_quote(raw_Date) + "\'"
+            except:
+                print("problem with ")
+            query = self.insert_release(name, artist, date)
+
+            self.__cur.execute(query)
+            db_dump = self.__cur.fetchone()
+            print(db_dump)
+        self.__connection.commit()
 
 
-def insert_release(name_Param, artist_Param, release_date_Param):
-    query =   """INSERT INTO release (name, artist, release_date) VALUES ({name}, {artist}, {release_date}) RETURNING name, artist;""".format(
-        name = name_Param, artist = artist_Param, release_date = release_date_Param)
-    return query
 
-def insert_genres(name_Param, artist_Param, genre_Param):
-    query =   """INSERT INTO genres (name, artist, genre) VALUES ({name}, {artist}, {genre}) RETURNING name, genre;""".format(
-        name = name_Param, artist = artist_Param, genre = genre_Param)
-    return query
+    def export_into_genres(self, genres):
+        for line in genres:
+            raw_Name = line.split(';')[0].strip()
+            name = "\'" + self.clear_single_quote(raw_Name) + "\'"
+            raw_Artist = line.split(';')[1].strip()
+            artist = "\'" + self.clear_single_quote(raw_Artist) + "\'"
+            raw_genre = line.split(';')[2].strip()
+            genre = "\'" + self.clear_single_quote(raw_genre) + "\'"
+            query = self.insert_genres(name, artist, genre)
+            self.__cur.execute(query)
+            db_dump = self.__cur.fetchone()
+            print(db_dump)
+        self.__connection.commit()
 
-def clean_Table(table_Name):
-    query = """DELETE from {name};""".format(name=table_Name)
-    return query
+
+    def clean_Table(self, table_Name):
+        query = """DELETE from {name};""".format(name=table_Name)
+        self.__cur.execute(query)
+    
+    def close_cursor(self):
+        self.__cur.close()
---
+++ data_from_playlist.py
@@ -0,0 +1,70 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials 
+from object_spotipy import aux_Spotipy as objsp
+import auxiliares_DB as aux_DB
+
+#spotify app authentication
+client_id = 'ID'
+client_secret = "Secret" 
+
+#Authentication - without user
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager) 
+
+#aux object init
+aux = objsp('ID', "Secret", sp)
+
+
+
+if __name__ == '__main__':
+    action = int(input("select 1 for dump release dates and genres per son into txt files" + '\n' + 
+                       "select 2 for exporting those dumps into a postgres database" + '\n' + ':'))
+    
+    if (action == 1):
+        #url de la playlist
+        playlist = input("URL de la playlist, dejar vacio para usar mi playlist: ")
+        if not playlist: 
+            playlist = "https://open.spotify.com/playlist/6DaFmf4BQVBRAonTp749vW?si=2b2c3e7b19af4357"
+
+        #cantidad de canciones a tomar
+        cantidad = int(input("Cantidad de canciones a contar: "))
+       
+       
+        try:
+            lista = aux.tracklist(playlist, cantidad)
+            valores = lista.values()
+        except:
+            raise Exception('no se pudo obtener la lista de {cant} canciones de la playlist {url}'.format(cant = cantidad, url = playlist))
+        results = aux.get_Playlist_Data(valores)
+        txt_genres = open('genres.txt', 'w')
+        txt_dates = open('dates.txt', 'w')
+        txt_dates.write("name; artist; date" + '\n')
+        txt_genres.write("name; artist; genre" + '\n')
+        
+        for num, result in enumerate(results):
+            try:
+                #descartar resultados erroneos
+                if result[0] == 's':
+                    continue
+                #quitar ";" de nombre de canciones
+                song_name = result[0].replace(';',' ')
+                artist = result[1]
+                genres = result[2]
+                release_date = result[3]
+                txt_dates.write(song_name + '; ' + artist + '; ' +  release_date + '\n')
+                for genre in genres:
+                    txt_genres.write(song_name + '; ' + artist + '; ' + str(genre) + '\n')
+            except:
+                print("write error with number " + str(num))
+    
+    elif (action == 2):
+        txt_genres = open('genres.txt',"r" )
+        txt_dates = open('dates.txt', "r")
+        db = aux_DB.db_user('database.ini', 'postgresql')
+        db.clean_Table('release')
+        db.clean_Table('genres')
+        db.export_into_release(txt_dates)
+        db.export_into_genres(txt_genres)
+        db.close_cursor()
+
+    
